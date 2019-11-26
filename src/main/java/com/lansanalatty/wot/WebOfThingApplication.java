320383cd54796ec11fe43c9d210942578f8835bb
lansanaconde <lansana.conde.pro@gmail.com>
2019-11-26 18:36:16 +0100

    first commit

---
+++ src/main/java/com/lansanalatty/wot/WebOfThingApplication.java
@@ -0,0 +1,12 @@
+package com.lansanalatty.wot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOfThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOfThingApplication.class, args);
+	}
+}
